ec01d17a714b771fa16f7c760b6a5eea8bb86bd9
salunke-gaurav007 <salunkegaurav209@gmail.com>
2025-04-14 16:38:46 +0530

    Update Experience.js

--- src/Data/Experience.js
+++ src/Data/Experience.js
@@ -2,12 +2,14 @@ export const Experience = [
     {
       title: "React Cohort",
       image: "/tutor.jpg",
-      description: "As a tutor in the Akatsuki React Cohort, I am responsible for teaching 100 students...",
+      description: "As a tutor in the Akatsuki React Cohort, I am responsible for teaching 100 students the fundamentals of JavaScript and TypeScript. The main goal of this event is to provide a strong foundation in React, and to ensure students have a solid understanding of core concepts in JavaScript and TypeScript before diving into React. My role involves breaking down complex topics into digestible lessons, empowering students to build their skills and confidence in web development.
+
+",
     },
     {
       title: "Reflection 2K25",
       image: "/2.jpg",
-      description: "As the Management Head for the annual function at RCPIT, Reflection 2K25, I oversee...",
+      description: "As the Management Head for the annual function at RCPIT, Reflection 2K25, I oversee the coordination of all pre-event preparations and handle on-the-day challenges to ensure the event's smooth execution. My role involves managing teams, addressing any issues that arise, and ensuring that the event runs efficiently and successfully, creating a memorable experience for all attendees.",
     },
   ];
   
