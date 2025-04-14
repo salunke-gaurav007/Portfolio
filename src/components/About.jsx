cc0d0a3dad9c9b7aa5023e1350f442faa180c2f3
salunke-gaurav007 <salunkegaurav209@gmail.com>
2025-04-14 16:35:18 +0530

    About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,8 @@ const About = () => (
         <div>
           <h2 className="text-2xl font-semibold mb-2">About Me</h2>
           <p>
-            I’m a second-year Computer Science student at RCPIT, Shirpur, with a growing passion...
+              I’m a second-year Computer Science student at RCPIT, Shirpur, with a growing passion for web development. I’ve worked with HTML, CSS, JavaScript, and have started exploring React as well. I’m also comfortable using MS Excel for organizing and managing data. I enjoy taking initiative—I'm an active member of the Akatsuki Coding Club and currently serve as the Management Head of the Reflection Club. Along with tech and management, I genuinely love public speaking and being on stage—it’s something that excites me just as much as coding. I enjoy learning new skills, working with driven teams, and stepping up whenever there's an opportunity to contribute or lead.
+         
           </p>
         </div>
       </div>
